127df2441d60f9564088872b68b4e00467885574
reut-git <rehutsofer@gmail.com>
2026-08-25 15:22:46 +0300

    Add LED pattern sequencer with button control

---
+++ LED Pattern Sequencer/LED_pattern_sequencer_with_button.ino
@@ -0,0 +1,58 @@
+constexpr int FOURTH_BIT = 2;
+constexpr int THIRD_BIT = 4;
+constexpr int SECOND_BIT = 5;
+constexpr int FIRST_BIT = 18;
+
+constexpr int BUTTON_PIN = 13;
+
+
+constexpr int leds[] = {
+    FIRST_BIT,
+    SECOND_BIT,
+    THIRD_BIT,
+    FOURTH_BIT
+};
+
+constexpr int Sequence[] = {
+    9, 6, 15, 0, 5, 10
+};
+
+
+
+constexpr int NUM_BITS = 4;
+constexpr int SEQUENCE_SIZE = 6;
+
+unsigned long lastChange = 0;
+
+int sequenceIndex = 0;
+
+void setup() {
+
+  pinMode(BUTTON_PIN, INPUT);
+
+  for(int i = 0 ; i < NUM_BITS ; i++){
+
+      pinMode(leds[i], OUTPUT);  
+  }
+
+}
+
+void loop() {
+  if(digitalRead(BUTTON_PIN) == HIGH){
+    
+      if(millis() - lastChange >= 500){
+
+      for (int i = 0 ; i < NUM_BITS ; i++) {
+          digitalWrite(leds[i], Sequence[sequenceIndex] & (1 << i) ? HIGH : LOW );
+      }
+
+      sequenceIndex++;
+        
+      if(sequenceIndex == SEQUENCE_SIZE) sequenceIndex = 0;
+
+      lastChange = millis();
+
+   }
+  } 
+     
+}
